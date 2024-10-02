237ec454b554b093411a81877fa871896935021c
Raunak Daga <rdaga@college.harvard.edu>
2024-10-02 09:37:53 -0400

    feat: ps3

--- fall2024/psets/ps2/ps2.py
+++ fall2024/psets/ps2/ps2.py
@@ -4,7 +4,7 @@ class BinarySearchTree:
     # key: int
     # item: int
     # size: int
-    def __init__(self, debugger = None):
+    def __init__(self, debugger=None):
         self.left = None
         self.right = None
         self.key = None
@@ -14,9 +14,8 @@ class BinarySearchTree:
 
     @property
     def size(self):
-         return self._size
-       
-     # a setter function
+        return self._size
+
     @size.setter
     def size(self, a):
         debugger = self.debugger
@@ -25,11 +24,12 @@ class BinarySearchTree:
         self._size = a
 
     ####### Part a #######
-    '''
+    """
     Calculates the size of the tree
     returns the size at a given node
-    '''
-    def calculate_sizes(self, debugger = None):
+    """
+
+    def calculate_sizes(self, debugger=None):
         # Debugging code
         # No need to modify
         # Provides counts
@@ -46,12 +46,13 @@ class BinarySearchTree:
             self.size += self.left.calculate_sizes(debugger)
         return self.size
 
-    '''
+    """
     Select the ind-th key in the tree
     
     ind: a number between 0 and n-1 (the number of nodes/objects)
     returns BinarySearchTree/Node or None
-    '''
+    """
+
     def select(self, ind):
         left_size = 0
         if self.left is not None:
@@ -61,51 +62,53 @@ class BinarySearchTree:
         if left_size > ind and self.left is not None:
             return self.left.select(ind)
         if left_size < ind and self.right is not None:
-            return self.right.select(ind)
+            # We need to adjust for the left subtree's nodes that we're no longer counting
+            return self.right.select(ind - left_size - 1)
         return None
 
-
-    '''
+    """
     Searches for a given key
     returns a pointer to the object with target key or None (Roughgarden)
-    '''
+    """
+
     def search(self, key):
+        # This is correct
         if self is None:
             return None
         elif self.key == key:
             return self
-        elif self.key < key and self.right is not None:
+        elif key > self.key and self.right is not None:
             return self.right.search(key)
-        elif self.left is not None:
+        elif key < self.key and self.left is not None:
             return self.left.search(key)
         return None
-    
 
-    '''
+    """
     Inserts a key into the tree
     key: the key for the new node; 
         ... this is NOT a BinarySearchTree/Node, the function creates one
     
     returns the original (top level) tree - allows for easy chaining in tests
-    '''
+    """
+
     def insert(self, key):
         if self.key is None:
             self.key = key
-        elif self.key > key: 
+        elif self.key > key:
             if self.left is None:
                 self.left = BinarySearchTree(self.debugger)
             self.left.insert(key)
+            self.size += 1
         elif self.key < key:
             if self.right is None:
                 self.right = BinarySearchTree(self.debugger)
             self.right.insert(key)
-        self.calculate_sizes()
+            self.size += 1
         return self
 
-    
     ####### Part b #######
 
-    '''
+    """
     Performs a `direction`-rotate the `side`-child of (the root of) T (self)
     direction: "L" or "R" to indicate the rotation direction
     child_side: "L" or "R" which child of T to perform the rotate on
@@ -125,15 +128,58 @@ class BinarySearchTree:
         12
         /
        11 
-    '''
+    """
+
     def rotate(self, direction, child_side):
-        # Your code goes here
+        # Algorithm
+        # Root is self
+
+        # Node we are choosing to rotate
+        # 'B'
+        rotation_node = self.right if child_side == "R" else self.left
+
+        # Prep for rotation
+        # y = 'D'
+        y = rotation_node.left if direction == "R" else rotation_node.right
+        if child_side == "R":
+            self.right = y
+        else:
+            self.left = y
+            # A.left = 'D'
+
+        # a, b, c are pointers that will be reassigned
+        if direction == "R":
+            a = rotation_node.right  # 'E'
+            b = y.right  # None
+            c = y.left  # None
+        else:
+            a = rotation_node.left
+            b = y.left
+            c = y.right
+
+        if direction == "R":
+            rotation_node.left = b  # 'B'.left = None
+            y.right = rotation_node  # 'D'.right = 'B'
+        else:
+            rotation_node.right = b
+            y.left = rotation_node
+
+        # sizes
+        if a:
+            y.size += 1 + a.size
+        else:
+            y.size += 1
+        if c:
+            rotation_node.size -= 1 + c.size
+        else:
+            rotation_node.size -= 1
+
         return self
 
     def print_bst(self):
         if self.left is not None:
             self.left.print_bst()
-        print( self.key),
+        print(self.key),
         if self.right is not None:
             self.right.print_bst()
         return self
--- fall2024/psets/ps3/simulator.py
+++ fall2024/psets/ps3/simulator.py
@@ -5,11 +5,12 @@
 from collections import defaultdict
 
 variableList = []
-# Note: defaultdict works exactly the same as a normal Python dictionary except it returns a default 
+# Note: defaultdict works exactly the same as a normal Python dictionary except it returns a default
 #       value (in this case, 0) when accessing a key that is not defined rather than raising KeyError.
-#       We are using a dictionary rather than a list/array to manage the memory so that we don't need to 
+#       We are using a dictionary rather than a list/array to manage the memory so that we don't need to
 #       initialize and store memory cells that are never accessed by the RAM program.
 memory = defaultdict(int)
+
 
 # Creates the variable list and the memory dictionary.
 # Initializes the 0th variable, input_len, to be the first element of the program array.
@@ -19,61 +20,69 @@ def setupEnv(programArr, inputArr):
 
     for i in range(programArr[0]):
         variableList.append(0)
-    
+
     variableList[0] = len(inputArr)
     for i in range(len(inputArr)):
         memory[i] = inputArr[i]
-        
+
+
 # Runs the given RAM program on the input.
 def executeProgram(programArr, inputArr):
     setupEnv(programArr, inputArr)
-    
+
     programArr = programArr[1:]
     programCounter = 0
     while programCounter < len(programArr):
         # Store the command and the list of operands.
         cmd = programArr[programCounter][0]
         ops = programArr[programCounter][1:]
-        
+
         # Assignment commands
-        if cmd == "read":       
-            # ['read', i, j]: lookup the var_j location in memory and assign that value to var_i                    
+        if cmd == "read":
+            # ['read', i, j]: lookup the var_j location in memory and assign that value to var_i
             variableList[ops[0]] = memory[variableList[ops[1]]]
         if cmd == "write":
-            # ['write', i, j]: store the value of var_j in memory at the location var_i 
+            # ['write', i, j]: store the value of var_j in memory at the location var_i
             memory[variableList[ops[0]]] = variableList[ops[1]]
         if cmd == "assign":
             # ['assign', i, j]: assign var_i to the value j
             # TODO: Implement assign.
-            pass
-            
+            variableList[ops[0]] = ops[1]
+
         # Arithmetic commands
         if cmd == "+":
             # ['+', i, j, k]: compute (var_j + var_k) and store in var_i
             # TODO: Implement addition.
-            pass
+            variableList[ops[0]] = variableList[ops[1]] + variableList[ops[2]]
         if cmd == "-":
             # ['-', i, j, k]: compute max((var_j - var_k), 0) and store in var_i.
             # TODO: Implement subtraction.
-            pass
+            variableList[ops[0]] = max(variableList[ops[1]] - variableList[ops[2]], 0)
         if cmd == "*":
             # ['*', i, j, k]: compute (var_j * var_k) and store in var_i.
             # TODO: Implement multiplication.
-            pass
+            variableList[ops[0]] = variableList[ops[1]] * variableList[ops[2]]
         if cmd == "/":
             #  ['/', i, j, k]: compute (var_j // var_k) and store in var_i.
             # Note that this is integer division. You should return an integer, not a float.
             # Remember division by 0 results in 0.
             # TODO: Implement division.
-            pass
-            
+            if variableList[ops[2]] == 0:
+                variableList[ops[0]] = 0
+            else:
+                variableList[ops[0]] = variableList[ops[1]] // variableList[ops[2]]
+
         # Control commands
         if cmd == "goto":
             # ['goto', i, j]: if var_i is equal to 0, go to line j
             # TODO: Implement goto.
-            pass
-        
+            if variableList[ops[0]] == 0:
+                programCounter = ops[1]
+                continue
+
         programCounter += 1
-    
+
     # Return the memory starting at output_ptr with length of output_len
-    return [memory[i] for i in range(variableList[1], variableList[1]+variableList[2])]
+    return [
+        memory[i] for i in range(variableList[1], variableList[1] + variableList[2])
+    ]
